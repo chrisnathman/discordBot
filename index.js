66ec161fca001c20f7a9bbe8f21ae896a9b16f6b
Christopher Nathman <nathmanc@gmail.com>
2020-05-24 04:07:20 -0700

    some additions to cmd processing

--- index.js
+++ index.js
@@ -25,8 +25,20 @@ dClient.once('disconnect', () => {
 
 dClient.on('message', async msg => {
     if (msg.author.bot) return;
-    if (!msg.content.startsWith(prefix)) return;
+    if (!msg.content.startsWith(prefix)) return
 
-    msg.channel.send('msg received')
+    //msg.channel.send('msg received')
     
+    const commandStr = msg.content.substring(2).trim()
+    const breakIndex = commandStr.indexOf(' ')
+    const cmd = commandStr.substring(0, breakIndex)
+    const args = commandStr.substring(breakIndex + 1)
+    if (breakIndex < 0)
+    {
+        
+    }
+    msg.channel.send('command: ' + cmd)
+    msg.channel.send('args: ' + args)
+
+ //   switch()
 })
